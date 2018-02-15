5e00d3b8baa5122e9aaaab33657501749182908d
Rozita Teymourzadeh <Rozita.Teymourzadeh@neonode.com>
2018-02-15 23:34:08 +0100

    remove arg as input

--- DetectionAnalysis.py
+++ DetectionAnalysis.py
@@ -3,7 +3,7 @@ Created on Jan 27, 2018
 
 @author: rozita.teymourzadeh
 '''
-import sys
+#import sys
 import time
 
 #class DetectionAnalysis(object):	
@@ -24,12 +24,12 @@ def banner(message,border='_'):
     print(message)
     print(line)
     
-def __calculation__(ref,target):
+def __calculation__():
     state=[]
     x_fw=[]
     y_fw=[]
-#		detection = open(r'C:\Users\Rozita.Teymourzadeh\Desktop\Accuracy\testData\RobotMeasurement_Detection.txt','r')
-    detection = open(target,"r")
+    detection = open(r'C:\Users\Rozita.Teymourzadeh\Desktop\Accuracy\testData\RobotMeasurement_Detection.txt','r')
+    #detection = open(target,'r')
     lines_fw = detection.readlines()
     detection.close()
     for line in lines_fw:
@@ -37,26 +37,30 @@ def __calculation__(ref,target):
 		state = line.split(";")[2]
 		x_fw = line.split(";")[3]
 		y_fw = line.split(";")[4]
-		print ( "state: ",state,"x_fw: ",x_fw,"y_fw: ", y_fw )
+    print ( "state: ",state,"x_fw: ",x_fw,"y_fw: ", y_fw )
 		
     x_rob=[]
     y_rob=[]
-#		robot = open(r'C:\Users\Rozita.Teymourzadeh\Desktop\Accuracy\testData\RobotMeasurement_RobotPositions.txt','r')
-    robot=open(ref)
+    robot = open(r'C:\Users\Rozita.Teymourzadeh\Desktop\Accuracy\testData\RobotMeasurement_RobotPositions.txt','r')
+    #robot=open(ref,'r')
     lines_robot = robot.readlines()
     robot.close()
     for line in lines_robot:
 		line = line.strip()
 		x_rob = line.split(";")[1]
 		y_rob = line.split(";")[2]
-		print ("x_rob: ","y_rob: ", x_rob,y_rob)
-def main(ref,target): 
-	   #DetectionAnalysis(ref,target)
-#	   __init__(ref,target)
-    __calculation__(ref,target)	
+    print ("x_rob: ","y_rob: ", x_rob,y_rob)
+        
+def main():
+    show_default() 
+    banner("Analytical tool execution started...") 
+    __calculation__()	
 	   	
 if __name__ == '__main__':
 	
-	main(sys.argv[1],sys.argv[2])
+	#main(sys.argv[1])
+    #ref = "r'C:\Users\Rozita.Teymourzadeh\Desktop\Accuracy\testData\RobotMeasurement_Detection.txt'"
+    #target = "r'C:\Users\Rozita.Teymourzadeh\Desktop\Accuracy\testData\RobotMeasurement_Detection.txt'"
+    main()
 
  
